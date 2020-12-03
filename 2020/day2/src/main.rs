50fd89c369367b656b8208712796e8c8d46874be
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-03 12:57:53 +0800

    day2 part2

--- 2020/day2/src/main.rs
+++ 2020/day2/src/main.rs
@@ -1,32 +1,33 @@
 use std::io::{self, BufRead};
 
-fn is_valid(min: u32, max: u32, t: char, s: &str) -> bool {
-    let mut count = 0;
-    for c in s.chars() {
-        if c == t {
-            count += 1;
-        }
-        if count > max {
-            return false;
-        }
-    }
-    return count >= min;
+fn is_valid_1(min: usize, max: usize, t: char, s: &str) -> bool {
+    (min..=max).contains(&s.chars().filter(|&c| c == t).count())
 }
 
+fn is_valid_2(p1: usize, p2: usize, t: char, s: &str) -> bool {
+    (s.chars().nth(p1 - 1).unwrap_or('-') == t) != (s.chars().nth(p2 - 1).unwrap_or('-') == t)
+}
+
+#[rustfmt::skip]
+fn valid_count(inputs: &Vec<(usize, usize, char, String)>, f: fn(usize, usize, char, &str) -> bool) -> usize {
+    inputs.iter().fold(0, |acc, (x, y, t, s)| acc + f(*x, *y, *t, s) as usize)
+}
+
+#[rustfmt::skip]
 fn main() {
-    let reader = io::stdin();
-    let result: u32 = reader.lock().lines().map(|l| l.unwrap()).fold(0, |acc, line| {
-        let mut parts = line
-            .split(|c| c == '-' || c == ' ' || c == ':')
-            .filter(|&s| !s.is_empty());
-        let min: u32 = parts.next().unwrap().parse().unwrap();
-        let max: u32 = parts.next().unwrap().parse().unwrap();
-        let t: char = parts.next().unwrap().parse().unwrap();
-        let s: &str = parts.next().unwrap();
-        match is_valid(min, max, t, s) {
-            true => acc + 1,
-            false => acc,
-        }
-    });
-    println!("part1 result: {}", result);
+    let inputs: Vec<_> = io::stdin().lock().lines().map(|l| l.unwrap())
+        .map(|line| {
+            let mut parts = line
+                .split(|c| c == '-' || c == ' ' || c == ':')
+                .filter(|&s| !s.is_empty());
+            let n1: usize = parts.next().unwrap().parse().unwrap();
+            let n2: usize = parts.next().unwrap().parse().unwrap();
+            let t:  char  = parts.next().unwrap().parse().unwrap();
+            let s:  &str  = parts.next().unwrap();
+            (n1, n2, t, s.to_owned())
+        })
+        .collect();
+
+    println!("part1 result: {}", valid_count(&inputs, is_valid_1));
+    println!("part2 result: {}", valid_count(&inputs, is_valid_2));
 }
