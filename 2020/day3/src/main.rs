9608adb1d39078f02292fc09b9407efaf4121286
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-03 14:43:20 +0800

    day3 part2

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -1,25 +1,32 @@
 use std::io::{self, BufRead};
 
 fn p1_solve(map: &Vec<Vec<char>>, direction: (usize, usize)) -> usize {
-    let (h, w) = (map.len(), map[0].len());
     let (dx, dy) = direction;
-    let (mut px, mut py) = (0, 0);
-    let mut count = 0;
-    while py < h {
-        count += (map[py][px] == '#') as usize;
-        px = (px + dx) % w;
-        py += dy;
-    }
-    count
+    let mut x = 0;
+    return map.iter().step_by(dy).fold(0, |acc, row| {
+        let acc = acc + (row[x] == '#') as usize;
+        x = (x + dx) % row.len();
+        acc
+    });
+}
+
+fn p2_solve(map: &Vec<Vec<char>>, slopes: &[(usize, usize)]) -> usize {
+    slopes.iter().fold(1, |acc, &direction| acc * p1_solve(map, direction))
 }
 
 fn main() {
-    let inputs: Vec<Vec<char>> = io::stdin()
-        .lock()
-        .lines()
+    #[rustfmt::skip]
+    let inputs: Vec<Vec<char>> = io::stdin().lock().lines()
         .map(|l| l.unwrap())
         .map(|line| line.chars().collect())
         .collect();
 
-    println!("part 1 result: {}", p1_solve(&inputs, (3, 1)));
+    let rs = p1_solve(&inputs, (3, 1));
+    println!("part 1 result: {}", rs);
+    assert_eq!(237, rs);
+
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let rs = p2_solve(&inputs, &slopes);
+    println!("part 2 result: {}", rs);
+    assert_eq!(2106818610, rs);
 }
