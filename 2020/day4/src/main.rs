128fb4c157a8a6a4f88b55d3db1d821322d52443
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-04 16:27:58 +0800

    day4 part1 - refactor

--- 2020/day4/src/main.rs
+++ 2020/day4/src/main.rs
@@ -1,15 +1,11 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::{self, Read};
 
-fn p1_solve(passports: &[&str], required: &HashSet<&str>) -> usize {
-    passports.iter().fold(0, |count, s| {
-        match s.split_whitespace().fold(0, |acc, item| match item.split(':').next() {
-            Some(field) => acc + required.contains(field) as usize,
-            None => acc,
-        }) {
-            7 => count + 1,
-            _ => count,
-        }
+fn p1_solve(passports: &[HashMap<&str, &str>]) -> usize {
+    passports.iter().fold(0, |count, passport| match passport.len() {
+        7 => count + !passport.contains_key("cid") as usize,
+        8 => count + 1,
+        _ => count,
     })
 }
 
@@ -17,16 +13,17 @@ fn main() {
     let reader = io::stdin();
     let mut inputs: String = String::new();
     reader.lock().read_to_string(&mut inputs).unwrap();
-    let inputs: Vec<_> = inputs.split("\n\n").filter(|s| !s.is_empty()).collect();
+    let inputs: Vec<_> = inputs.split("\n\n").filter(|s| !s.is_empty()).map(parse).collect();
 
-    let mut required = HashSet::new();
-    required.insert("byr");
-    required.insert("iyr");
-    required.insert("eyr");
-    required.insert("hgt");
-    required.insert("hcl");
-    required.insert("ecl");
-    required.insert("pid");
+    println!("part 1 result: {}", p1_solve(&inputs));
+}
 
-    println!("part 1 result: {}", p1_solve(&inputs, &required));
+#[rustfmt::skip]
+fn parse(s: &str) -> HashMap<&str, &str> {
+    return s.split_whitespace().filter(|s| !s.is_empty())
+        .map(|item| {
+            let mut split = item.split(":");
+            (split.next().unwrap(), split.next().unwrap())
+        })
+        .collect();
 }
