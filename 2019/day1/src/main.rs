db6519bc89008df6dad677a1a3b67904a001935c
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-05 11:36:10 +0800

    2019 - day1 part2

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -1,17 +1,33 @@
 use std::io::{self, BufRead};
 
-fn p1_solve(modules: &[usize]) -> usize {
+fn p1_solve(modules: &[i64]) -> i64 {
     modules.iter().fold(0, |acc, m| m / 3 - 2 + acc)
 }
 
+#[rustfmt::skip]
+fn p2_solve(modules: &[i64]) -> i64 {
+    modules.iter().fold(0, |acc, &m| {
+        let (mut m, mut total) = (m, 0);
+        loop {
+            m = m / 3 - 2;
+            if m < 0 { break }
+            total += m;
+        }
+        acc + total
+    })
+}
+
+#[rustfmt::skip]
 fn main() {
-    let inputs: Vec<usize> = io::stdin()
-        .lock()
-        .lines()
+    let inputs: Vec<i64> = io::stdin().lock().lines()
         .map(|l| l.unwrap().parse().unwrap())
         .collect();
 
     let result = p1_solve(&inputs);
     println!("part1 result: {}", result);
     assert_eq!(3455717, result);
+
+    let result = p2_solve(&inputs);
+    println!("part2 result: {}", result);
+    assert_eq!(5180690, result);
 }
