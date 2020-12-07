2919cedff0f74a3d7b3d8ddc6f58c9b9f6369583
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-07 11:31:01 +0800

    2020 - day6 part2 - refactor

--- 2020/day6/src/main.rs
+++ 2020/day6/src/main.rs
@@ -1,32 +1,29 @@
 use std::io::{self, Read};
 
-fn p1_count(answers: &[&str]) -> usize {
-    let mut questions = [0; 26];
-    answers.iter().flat_map(|s| s.as_bytes()).for_each(|&c| {
-        questions[(c - b'a') as usize] = 1;
-    });
-    questions.iter().sum()
-}
-
-fn p2_count(answers: &[&str]) -> usize {
+fn count<F>(answers: &[&str], f: F) -> usize
+where
+    F: Fn(usize) -> bool,
+{
     let mut questions = [0; 26];
     answers.iter().flat_map(|s| s.as_bytes()).for_each(|&c| {
         questions[(c - b'a') as usize] += 1;
     });
-    questions.iter().filter(|&&n| n == answers.len()).count()
+    questions.iter().filter(|&&n| f(n)).count()
 }
 
+#[rustfmt::skip]
 fn main() {
     let mut inputs = String::new();
     io::stdin().read_to_string(&mut inputs).unwrap();
-    let inputs: Vec<Vec<_>> = inputs
-        .split("\n\n")
+    let inputs: Vec<Vec<_>> = inputs.split("\n\n")
         .map(|p| p.split('\n').filter(|s| !s.is_empty()).collect())
         .collect();
 
-    let result: usize = inputs.iter().map(|v| p1_count(&v)).sum();
+    let result: usize = inputs.iter().map(|v| count(&v, |n| n > 0)).sum();
     println!("part 1 result: {}", result);
+    assert_eq!(6416, result);
 
-    let result: usize = inputs.iter().map(|v| p2_count(&v)).sum();
+    let result: usize = inputs.iter().map(|v| count(&v, |n| n == v.len())).sum();
     println!("part 2 result: {}", result);
+    assert_eq!(3050, result);
 }
