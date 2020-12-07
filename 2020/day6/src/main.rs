0d05005fc2e58cda55925ec380d396b4a0d2dea2
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-07 11:16:58 +0800

    2020 - day6 part2

--- 2020/day6/src/main.rs
+++ 2020/day6/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Read};
 
-fn questions_count(answers: &[&str]) -> usize {
+fn p1_count(answers: &[&str]) -> usize {
     let mut questions = [0; 26];
     answers.iter().flat_map(|s| s.as_bytes()).for_each(|&c| {
         questions[(c - b'a') as usize] = 1;
@@ -8,11 +8,25 @@ fn questions_count(answers: &[&str]) -> usize {
     questions.iter().sum()
 }
 
+fn p2_count(answers: &[&str]) -> usize {
+    let mut questions = [0; 26];
+    answers.iter().flat_map(|s| s.as_bytes()).for_each(|&c| {
+        questions[(c - b'a') as usize] += 1;
+    });
+    questions.iter().filter(|&&n| n == answers.len()).count()
+}
+
 fn main() {
     let mut inputs = String::new();
     io::stdin().read_to_string(&mut inputs).unwrap();
-    let inputs: Vec<Vec<_>> = inputs.split("\n\n").map(|p| p.split('\n').collect()).collect();
+    let inputs: Vec<Vec<_>> = inputs
+        .split("\n\n")
+        .map(|p| p.split('\n').filter(|s| !s.is_empty()).collect())
+        .collect();
 
-    let result: usize = inputs.iter().map(|v| questions_count(&v)).sum();
+    let result: usize = inputs.iter().map(|v| p1_count(&v)).sum();
     println!("part 1 result: {}", result);
+
+    let result: usize = inputs.iter().map(|v| p2_count(&v)).sum();
+    println!("part 2 result: {}", result);
 }
