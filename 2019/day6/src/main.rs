be59617c1ddd83659c84413e68fc091dddd0bf53
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-08 17:18:26 +0800

    2019 - day6 part2

--- 2019/day6/src/main.rs
+++ 2019/day6/src/main.rs
@@ -7,6 +7,27 @@ fn p1_solve(orbits: &HashMap<String, String>) -> usize {
     orbits.iter().map(|(child, _)| orbit_depth(orbits, &mut cache, child)).sum()
 }
 
+#[rustfmt::skip]
+fn p2_solve(orbits: &HashMap<String, String>) -> usize {
+    let you_path = orbit_path(orbits, "YOU");
+    let san_path = orbit_path(orbits, "SAN");
+    let common_path = you_path.iter().rev()
+        .zip(san_path.iter().rev())
+        .take_while(|(you, san)| you == san)
+        .count();
+    you_path.len() + san_path.len() - 2 * common_path
+}
+
+fn orbit_path(orbits: &HashMap<String, String>, star: &str) -> Vec<String> {
+    let mut path = Vec::new();
+    let mut star = star;
+    while let Some(parent) = orbits.get(star) {
+        path.push(parent.to_owned());
+        star = parent;
+    }
+    path
+}
+
 fn orbit_depth(orbits: &HashMap<String, String>, cache: &mut HashMap<String, usize>, star: &str) -> usize {
     match cache.get(star) {
         Some(&depth) => depth,
@@ -36,4 +57,8 @@ fn main() {
     let result = p1_solve(&inputs);
     println!("part1 result: {}", result);
     assert_eq!(117672, result);
+
+    let result = p2_solve(&inputs);
+    println!("part2 result: {}", result);
+    assert_eq!(277, result);
 }
