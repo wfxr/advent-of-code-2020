0bf692e82e0b71693249c8e92e03a971ddffd6bb
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-08 12:56:16 +0800

    2019 - day5 part1 - refactor

--- 2019/day5/src/main.rs
+++ 2019/day5/src/main.rs
@@ -1,67 +1,104 @@
 use std::io::{self, BufRead};
 
-fn p1_solve(intcode: &[i64], input: i64) -> i64 {
-    let mut intcode = intcode.to_vec();
-    let mut result = 0;
-    let mut p = 0;
-    while p < intcode.len() {
-        let mut modes = 1;
-        let head = load(&intcode, &mut p, &mut modes) as usize;
-        let (mut modes, opcode) = (head / 100, head % 100);
-        match opcode {
-            1 => {
-                let a = load(&intcode, &mut p, &mut modes);
-                let b = load(&intcode, &mut p, &mut modes);
-                store(&mut intcode, &mut p, a + b);
-            }
-            2 => {
-                let a = load(&intcode, &mut p, &mut modes);
-                let b = load(&intcode, &mut p, &mut modes);
-                store(&mut intcode, &mut p, a * b);
-            }
-            3 => store(&mut intcode, &mut p, input),
-            4 => result = output(&mut intcode, &mut p),
-            99 => break,
-            _ => {
-                println!("{}", opcode);
-                println!("{}", p);
-                unreachable!();
-            }
+struct IntcodeComputer {
+    program: Vec<i64>,
+    pos:     usize,
+    modes:   usize,
+    opcode:  usize,
+}
+
+impl IntcodeComputer {
+    pub fn new(program: &[i64]) -> Self {
+        IntcodeComputer {
+            program: program.to_vec(),
+            pos:     0,
+            modes:   0,
+            opcode:  0,
         }
     }
-    result
-}
 
-fn output(intcode: &mut [i64], p: &mut usize) -> i64 {
-    let value = intcode[intcode[*p] as usize];
-    *p += 1;
-    return value;
-}
+    pub fn run(&mut self, input: i64) -> i64 {
+        let mut res = 0;
+        while !self.finished() {
+            self.load_instruction();
+            match self.opcode {
+                1 => {
+                    let (a, b) = (self.load_param(), self.load_param());
+                    self.save(a + b);
+                }
+                2 => {
+                    let (a, b) = (self.load_param(), self.load_param());
+                    self.save(a * b);
+                }
+                3 => self.save(input),
+                4 => res = self.deref_load(),
+                99 => break,
+                _ => unreachable!("unknown opcode {} (pos: {})", self.opcode, self.pos),
+            }
+        }
+        res
+    }
 
-fn store(intcode: &mut [i64], p: &mut usize, value: i64) {
-    intcode[intcode[*p] as usize] = value;
-    *p += 1;
-}
-fn load(intcode: &[i64], p: &mut usize, modes: &mut usize) -> i64 {
-    let value = intcode[*p];
-    let value = match *modes % 10 {
-        0 => intcode[value as usize],
-        1 => value,
-        _ => unreachable!(),
-    };
-    *modes /= 10;
-    *p += 1;
-    value
+    pub fn finished(&self) -> bool {
+        self.pos >= self.program.len()
+    }
+
+    fn load(&mut self) -> i64 {
+        let res = self.program[self.pos];
+        self.pos += 1;
+        res
+    }
+
+    fn deref_load(&mut self) -> i64 {
+        let addr = self.load() as usize;
+        self.program[addr]
+    }
+
+    fn load_param(&mut self) -> i64 {
+        let data = match self.modes % 10 {
+            0 => self.deref_load(),
+            1 => self.load(),
+            _ => unreachable!("unknown modes {} (pos: {})", self.modes, self.pos),
+        };
+        self.modes /= 10;
+        data
+    }
+
+    fn load_instruction(&mut self) {
+        let instruction = self.load() as usize;
+        self.modes = instruction / 100;
+        self.opcode = instruction % 100;
+    }
+
+    fn save(&mut self, value: i64) {
+        let addr = self.load() as usize;
+        self.program[addr] = value;
+    }
 }
 
 #[rustfmt::skip]
 fn main() {
+    test(&[1,0,0,0,99],          0,   &[2,0,0,0,99],          0);
+    test(&[2,3,0,3,99],          0,   &[2,3,0,6,99],          0);
+    test(&[2,4,4,5,99,0],        0,   &[2,4,4,5,99,9801],     0);
+    test(&[1,1,1,4,99,5,6,0,99], 0,   &[30,1,1,4,2,5,6,0,99], 0);
+    test(&[3,0,4,0,99],          302, &[302,0,4,0,99],        302);
+
     let inputs: Vec<_> = io::stdin().lock().lines().next().unwrap().unwrap()
         .split(',')
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let result = p1_solve(&inputs , 1);
+    let mut computer = IntcodeComputer::new(&inputs);
+    let result = computer.run(1);
     println!("part 1 result: {}", result);
     assert_eq!(8332629, result);
+}
+
+fn test(program: &[i64], input: i64, expected_program: &[i64], expected_output: i64) {
+    let mut computer = IntcodeComputer::new(program);
+    let actual_output = computer.run(input);
+
+    assert_eq!(expected_program, computer.program);
+    assert_eq!(expected_output, actual_output);
 }
