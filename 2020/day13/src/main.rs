e17192ef7e30b50c0f9190c2f2b76692d5ae9e01
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-14 16:37:46 +0800

    2020 - day13 part2

--- 2020/day13/src/main.rs
+++ 2020/day13/src/main.rs
@@ -1,8 +1,25 @@
 use std::io::{self, BufRead};
 
+#[rustfmt::skip]
 fn p1_solve(schedule: &[&str], t: usize) -> (usize, usize) {
-    let schedule = schedule.iter().filter(|&&s| s != "x").map(|bus| bus.parse().unwrap());
-    schedule.map(|bus| (bus - t % bus, bus)).min().unwrap()
+    schedule.iter()
+        .filter_map(|s| s.parse().ok())
+        .map(|bus| (bus - t % bus, bus))
+        .min().unwrap()
+}
+
+#[rustfmt::skip]
+fn p2_solve(schedule: &[&str]) -> usize {
+    schedule.iter().enumerate()
+        .fold((1, 1), |(mut t, mut step), (delay, &s)| {
+            if let Ok(bus) = s.parse::<usize>() {
+                while (t + delay) % bus != 0 {
+                    t += step;
+                }
+                step *= bus;
+            }
+            (t, step)
+        }).0
 }
 
 fn main() {
@@ -12,7 +29,11 @@ fn main() {
     let schedule = lines.next().unwrap().unwrap();
     let schedule: Vec<_> = schedule.split(',').collect();
 
-    let result = p1_solve(&schedule, t);
-    println!("part 1 result: {} {:?}", result.0 * result.1, result);
-    assert_eq!(2305, result.0 * result.1);
+    let (wait, bus) = p1_solve(&schedule, t);
+    println!("part 1 result: {}", wait * bus);
+    assert_eq!(2305, wait * bus);
+
+    let result = p2_solve(&schedule);
+    println!("part 2 result: {}", result);
+    assert_eq!(552612234243498, result);
 }
