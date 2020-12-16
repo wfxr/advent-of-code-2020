676810d21b4c5aa44c7c28db4d3e633e1a59d62b
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-16 11:33:11 +0800

    2020 - day15 part2 - refactor

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,19 +1,16 @@
+#[rustfmt::skip]
 fn solve(nums: &[usize], nth: usize) -> usize {
-    let mut position = vec![(0, 0); nth];
-    nums.iter().enumerate().for_each(|(i, &n)| position[n] = (0, i + 1));
-    let mut prev = *nums.last().unwrap();
-    for i in nums.len()..nth {
-        prev = match position[prev] {
-            (0, _) => 0,
-            (p1, p2) => p2 - p1,
-        };
-        position[prev] = (position[prev].1, i + 1);
-    }
-    prev
+    let mut position = vec![0; nth];
+    nums.iter().enumerate().for_each(|(i, &n)| position[n] = i + 1);
+    (nums.len()..nth).fold(*nums.last().unwrap(), |prev, i| {
+        let t = position[prev];
+        position[prev] = i;
+        if t == 0 { 0 } else { i - t }
+    })
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let inputs: Vec<usize> = std::fs::read_to_string("inputs.txt")?
+fn main() {
+    let inputs: Vec<usize> = include_str!("../inputs.txt")
         .split(',')
         .map(|s| s.trim().parse().unwrap())
         .collect();
@@ -22,8 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("part 1 result: {}", result);
     assert_eq!(866, result);
 
-    let result = solve(&inputs, 30000000);
-    println!("part 1 result: {}", result);
+    let result = solve(&inputs, 30_000_000);
+    println!("part 2 result: {}", result);
     assert_eq!(1437692, result);
-    Ok(())
 }
