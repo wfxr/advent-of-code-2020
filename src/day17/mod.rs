62cf032fa64e1435b8b58d3db3fd5178e5de76b5
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-18 11:54:46 +0800

    refactor: day17

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -3,13 +3,12 @@ use std::collections::HashSet;
 
 type Point3D = (i32, i32, i32);
 type Point4D = (i32, i32, i32, i32);
-type Range = std::ops::Range<i32>;
 
 fn parse_input(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn unit3d_actived(matrix: &HashSet<Point3D>, (x, y, z): Point3D) -> usize {
+fn unit3d_actived(matrix: &HashSet<Point3D>, (x, y, z): &Point3D) -> usize {
     let mut count = 0;
     for dx in -1..2 {
         for dy in -1..2 {
@@ -21,7 +20,7 @@ fn unit3d_actived(matrix: &HashSet<Point3D>, (x, y, z): Point3D) -> usize {
     count
 }
 
-fn unit4d_actived(matrix: &HashSet<Point4D>, (x, y, z, w): Point4D) -> usize {
+fn unit4d_actived(matrix: &HashSet<Point4D>, (x, y, z, w): &Point4D) -> usize {
     let mut count = 0;
     for dx in -1..2 {
         for dy in -1..2 {
@@ -35,51 +34,64 @@ fn unit4d_actived(matrix: &HashSet<Point4D>, (x, y, z, w): Point4D) -> usize {
     count
 }
 
-fn part1_proceed(curr: &mut HashSet<Point3D>, (range_x, range_y, range_z): &mut (Range, Range, Range)) {
+fn part1_proceed(curr: &mut HashSet<Point3D>) {
     let mut next = curr.clone();
-    for x in range_x.clone() {
-        for y in range_y.clone() {
-            for z in range_z.clone() {
-                let p = (x, y, z);
-                match (curr.contains(&p), unit3d_actived(&curr, p)) {
-                    (_, 3) | (true, 4) => next.insert(p),
-                    _ => next.remove(&p),
-                };
-            }
-        }
-    }
-    range_x.start -= 1;
-    range_x.end += 1;
-    range_y.start -= 1;
-    range_y.end += 1;
-    range_z.start -= 1;
-    range_z.end += 1;
-    *curr = next;
-}
-
-fn part2_proceed(curr: &mut HashSet<Point4D>, (range_x, range_y, range_z, range_w): &mut (Range, Range, Range, Range)) {
-    let mut next = curr.clone();
-    for x in range_x.clone() {
-        for y in range_y.clone() {
-            for z in range_z.clone() {
-                for w in range_w.clone() {
-                    let p = (x, y, z, w);
-                    match (curr.contains(&p), unit4d_actived(&curr, p)) {
-                        (_, 3) | (true, 4) => next.insert(p),
-                        _ => next.remove(&p),
+    let mut calculated = HashSet::new();
+    curr.iter().for_each(|(x, y, z)| {
+        for dx in -1..2 {
+            for dy in -1..2 {
+                for dz in -1..2 {
+                    let p = (x + dx, y + dy, z + dz);
+                    if calculated.contains(&p) {
+                        continue;
+                    }
+                    let state = curr.contains(&p);
+                    let next_state = next_state(state, unit3d_actived(&curr, &p));
+                    match (state, next_state) {
+                        (true, false) => next.remove(&p),
+                        (false, true) => next.insert(p),
+                        _ => false,
                     };
+                    calculated.insert(p);
                 }
             }
         }
+    });
+    *curr = next;
+}
+
+fn next_state(state: bool, count: usize) -> bool {
+    match state {
+        true => count == 3 || count == 4,
+        false => count == 3,
     }
-    range_x.start -= 1;
-    range_x.end += 1;
-    range_y.start -= 1;
-    range_y.end += 1;
-    range_z.start -= 1;
-    range_z.end += 1;
-    range_w.start -= 1;
-    range_w.end += 1;
+}
+
+fn part2_proceed(curr: &mut HashSet<Point4D>) {
+    let mut next = curr.clone();
+    let mut calculated = HashSet::new();
+    curr.iter().for_each(|(x, y, z, w)| {
+        for dx in -1..2 {
+            for dy in -1..2 {
+                for dz in -1..2 {
+                    for dw in -1..2 {
+                        let p = (x + dx, y + dy, z + dz, w + dw);
+                        if calculated.contains(&p) {
+                            continue;
+                        }
+                        let state = curr.contains(&p);
+                        let next_state = next_state(state, unit4d_actived(&curr, &p));
+                        match (state, next_state) {
+                            (true, false) => next.remove(&p),
+                            (false, true) => next.insert(p),
+                            _ => false,
+                        };
+                        calculated.insert(p);
+                    }
+                }
+            }
+        }
+    });
     *curr = next;
 }
 
@@ -94,15 +106,9 @@ pub(super) const SOLUTION: Solution = Solution {
                 }
             }
         }
-
-        let range_x = -1..(input[0].len() as i32 + 1);
-        let range_y = -1..(input.len() as i32 + 1);
-        let range_z = -1..2;
-        let mut range = (range_x, range_y, range_z);
         for _ in 0..6 {
-            part1_proceed(&mut matrix, &mut range);
+            part1_proceed(&mut matrix);
         }
-
         let result = matrix.len();
         Ok(result.to_string())
     },
@@ -116,16 +122,9 @@ pub(super) const SOLUTION: Solution = Solution {
                 }
             }
         }
-
-        let range_x = -1..(input[0].len() as i32 + 1);
-        let range_y = -1..(input.len() as i32 + 1);
-        let range_z = -1..2;
-        let range_w = -1..2;
-        let mut range = (range_x, range_y, range_z, range_w);
         for _ in 0..6 {
-            part2_proceed(&mut matrix, &mut range);
+            part2_proceed(&mut matrix);
         }
-
         let result = matrix.len();
         Ok(result.to_string())
     },
