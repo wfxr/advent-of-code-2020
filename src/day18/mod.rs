2748fe27e70007724b1e7d8b9a2942b8c1e3e9f6
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-18 20:05:09 +0800

    refactor: day18

--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -1,35 +1,28 @@
 use crate::Solution;
-use ExprNode::{Operand, Operator};
 
-#[derive(Debug)]
-enum ExprNode {
-    Operand(i64),
-    Operator(char),
-}
-
-fn to_postfix(expr: &str, prec: fn(char) -> u8) -> Vec<ExprNode> {
+fn to_postfix(expr: &str, prec: fn(char) -> u8) -> Vec<char> {
     let mut st = vec![];
     let mut rs = vec![];
     for c in expr.chars() {
         match c {
             ' ' => continue,
-            '0'..='9' => rs.push(Operand(c as i64 - '0' as i64)),
-            '(' => st.push(Operator(c)),
+            '0'..='9' => rs.push(c),
+            '(' => st.push(c),
             ')' => loop {
                 match st.pop() {
-                    Some(Operator('(')) | None => break,
+                    Some('(') | None => break,
                     Some(op) => rs.push(op),
                 };
             },
             op => {
                 loop {
                     match st.last() {
-                        Some(&Operator('(')) => break,
-                        Some(&Operator(top)) if prec(top) <= prec(op) => rs.push(st.pop().unwrap()),
+                        Some('(') => break,
+                        Some(&top) if prec(top) <= prec(op) => rs.push(st.pop().unwrap()),
                         _ => break,
                     }
                 }
-                st.push(Operator(op))
+                st.push(op)
             }
         }
     }
@@ -37,19 +30,18 @@ fn to_postfix(expr: &str, prec: fn(char) -> u8) -> Vec<ExprNode> {
     rs
 }
 
-fn evaluate(expr: &[ExprNode]) -> i64 {
+fn evaluate(expr: &[char]) -> i64 {
     let mut st = vec![];
-    for node in expr {
-        match node {
-            &Operand(val) => st.push(val),
-            operator => {
+    for c in expr {
+        match c {
+            '0'..='9' => st.push(*c as i64 - '0' as i64),
+            op => {
                 let (rhs, lhs) = (st.pop().unwrap(), st.pop().unwrap());
-                let val = match operator {
-                    Operator('+') => lhs + rhs,
-                    Operator('*') => lhs * rhs,
-                    _ => panic!(format!("unexpected operator: {:?}", operator)),
-                };
-                st.push(val);
+                st.push(match op {
+                    '+' => lhs + rhs,
+                    '*' => lhs * rhs,
+                    _ => panic!(format!("unexpected operator: {:?}", op)),
+                });
             }
         }
     }
