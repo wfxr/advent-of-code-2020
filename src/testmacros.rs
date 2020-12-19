dfff8fe9dc32518b0607aff7259503fcd88f58f2
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-19 15:59:07 +0800

    feat: new test macro

--- src/testmacros.rs
+++ src/testmacros.rs
@@ -1,23 +1,30 @@
 #[macro_export]
+macro_rules! test {
+    ($part:ident $(=> $answer:expr)? $(,$name:ident: $input:expr => $expected:expr)*) => {
+        $(
+            #[test]
+            fn $part() {
+                let input = include_str!("input");
+                let res = (super::SOLUTION.$part)(&input).unwrap();
+                assert_eq!(stringify!($answer), res);
+            }
+        )?
+        $(
+            #[test]
+            fn $name() {
+                let res = (super::SOLUTION.$part)($input).unwrap();
+                assert_eq!(res, stringify!($expected));
+            }
+        )*
+    };
+}
+
+#[macro_export]
 macro_rules! solution_test {
     ($part1:expr, $part2:expr) => {
         mod test {
-            mod solution {
-                use super::super::*;
-                #[test]
-                fn part1() {
-                    let input = include_str!("input");
-                    let res = (SOLUTION.part1)(&input).unwrap();
-                    assert_eq!(res, stringify!($part1));
-                }
-
-                #[test]
-                fn part2() {
-                    let input = include_str!("input");
-                    let res = (SOLUTION.part2)(&input).unwrap();
-                    assert_eq!(res, stringify!($part2));
-                }
-            }
+            crate::test!(part1 => $part1);
+            crate::test!(part2 => $part2);
         }
     };
 }
