e7761cd5186d38c0aa38600e6f8455c91dde47db
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-19 17:23:20 +0800

    refactor: improve macros

--- src/testmacros.rs
+++ src/testmacros.rs
@@ -1,21 +1,26 @@
 #[macro_export]
 macro_rules! test {
-    ($part:ident $(=> $answer:expr)? $(,$name:ident: $input:expr => $expected:expr)*) => {
-        $(
-            #[test]
-            fn $part() {
-                let input = include_str!("input");
-                let res = (super::SOLUTION.$part)(&input).unwrap();
-                assert_eq!(stringify!($answer), res);
-            }
-        )?
-        $(
-            #[test]
-            fn $name() {
-                let res = (super::SOLUTION.$part)($input).unwrap();
-                assert_eq!(res, stringify!($expected));
-            }
-        )*
+    ($part:ident $(=> $answer:expr)? $(,$name:ident: $input:expr => $expected:expr)* $(,)?) => {
+        mod $part {
+            use super::super::SOLUTION;
+            #[allow(unused_imports)]
+            use crate::input;
+            $(
+                #[test]
+                fn answer() {
+                    let input = include_str!("input");
+                    let res = (SOLUTION.$part)(&input).unwrap();
+                    assert_eq!(stringify!($answer), res);
+                }
+            )?
+            $(
+                #[test]
+                fn $name() {
+                    let res = (SOLUTION.$part)($input).unwrap();
+                    assert_eq!(res, stringify!($expected));
+                }
+            )*
+        }
     };
 }
 
@@ -27,4 +32,10 @@ macro_rules! solution_test {
             crate::test!(part2 => $part2);
         }
     };
+}
+
+#[macro_export]
+macro_rules! input {
+    ($line:expr) => { $line };
+    ($line:expr, $($rest:expr),+ $(,)?) => { concat!($line, '\n', input!($($rest),+)) };
 }
