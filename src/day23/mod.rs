1f106bd1b642cf7498bb06a3e19abc6c700d0f2f
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-24 11:31:58 +0800

    perf: day23

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -1,39 +1,40 @@
 use crate::{solution, Result};
 
-fn solve(cups: &mut Vec<usize>, moves: usize) {
+fn solve(cups: &mut [u32], moves: usize) {
     for _ in 0..moves {
         let a0 = cups[0];
-        let a1 = cups[a0];
-        let a2 = cups[a1];
-        let a3 = cups[a2];
+        let a1 = cups[a0 as usize];
+        let a2 = cups[a1 as usize];
+        let a3 = cups[a2 as usize];
+        let a4 = cups[a3 as usize];
         let mut t = a0;
         while t == a0 || t == a1 || t == a2 || t == a3 {
-            t = if t > 1 { t - 1 } else { t + cups.len() - 2 }
+            t = if t > 1 { t - 1 } else { t + cups.len() as u32 - 2 }
         }
-        cups[a0] = cups[a3];
-        cups[0] = cups[a3];
-        let tnext = cups[t];
-        cups[t] = a1;
-        cups[a3] = tnext;
+        cups[a0 as usize] = a4;
+        cups[0] = a4;
+        let tnext = cups[t as usize];
+        cups[t as usize] = a1;
+        cups[a3 as usize] = tnext;
     }
 }
 
-fn parse_input(input: &str, total_cups: Option<usize>) -> Vec<usize> {
-    let input: Vec<_> = input.trim().chars().map(|c| c as usize - '0' as usize).collect();
+fn parse_input(input: &str, total_cups: Option<usize>) -> Vec<u32> {
+    let input: Vec<_> = input.trim().chars().filter_map(|c| c.to_digit(10)).collect();
     let total_cups = if let Some(n) = total_cups { n } else { input.len() };
     let mut cups = vec![0; total_cups + 1];
     let mut x = 0;
     for &n in &input {
-        cups[x] = n;
+        cups[x as usize] = n;
         x = n;
     }
-    cups[input[input.len() - 1]] = if input.len() == total_cups {
+    cups[input[input.len() - 1] as usize] = if input.len() == total_cups {
         input[0]
     } else {
-        input.len() + 1
+        input.len() as u32 + 1
     };
-    (input.len() + 1..cups.len()).for_each(|x| {
-        cups[x] = x + 1;
+    (input.len() + 1..cups.len()).for_each(|i| {
+        cups[i] = i as u32 + 1;
     });
     if total_cups > input.len() {
         cups[total_cups] = input[0];
@@ -45,11 +46,11 @@ fn parse_input(input: &str, total_cups: Option<usize>) -> Vec<usize> {
 fn part1(input: &str) -> Result<usize> {
     let mut cups = parse_input(input, None);
     solve(&mut cups, 100);
-    let mut sum = 0;
-    let mut cup = 1;
-    while cups[cup] != 1 {
-        sum = sum * 10 + cups[cup];
-        cup = cups[cup];
+    let mut sum = 0usize;
+    let mut cup = 1u32;
+    while cups[cup as usize] != 1 {
+        sum = sum * 10 + cups[cup as usize] as usize;
+        cup = cups[cup as usize];
     }
     Ok(sum)
 }
@@ -58,7 +59,7 @@ fn part1(input: &str) -> Result<usize> {
 fn part2(input: &str) -> Result<usize> {
     let mut cups = parse_input(input, Some(1_000_000));
     solve(&mut cups, 10_000_000);
-    Ok(cups[1] * cups[cups[1]])
+    Ok(cups[1] as usize * cups[cups[1] as usize] as usize)
 }
 
 solution!(part1 => 49725386, part2 => 538935646702);
