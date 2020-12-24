f828927cb9fcd87372b6a212b6c17624907039bb
Wenxuan Zhang <wenxuangm@gmail.com>
2020-12-24 12:26:46 +0800

    perf: day15

--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -1,30 +1,26 @@
 use crate::{solution, Result};
 
 #[rustfmt::skip]
-fn solve(nums: &[usize], nth: usize) -> Result<usize> {
-    let mut position = vec![0; nth];
-    nums.iter().enumerate().for_each(|(i, &n)| position[n] = i + 1);
-    Ok(
-        (nums.len()..nth).fold(*nums.last().ok_or("input is empty")?, |prev, i| {
-            let t = position[prev];
-            position[prev] = i;
-            if t == 0 { 0 } else { i - t }
-        })
-    )
+fn solve(nums: &[u32], nth: u32) -> Result<u32> {
+    let mut position = vec![0u32; nth as usize];
+    nums.iter().enumerate().for_each(|(i, &n)| position[n as usize] = i as u32 + 1);
+    let res = (nums.len() as u32..nth).fold(*nums.last().ok_or("input is empty")?, |prev, i| {
+        let t = position[prev as usize];
+        position[prev as usize] = i;
+        if t == 0 { 0 } else { i - t }
+    });
+    Ok(res)
 }
 
-fn parse_input(input: &str) -> Result<Vec<usize>> {
-    input
-        .split(',')
-        .map(|s| s.trim().parse::<usize>().map_err(|e| e.into()))
-        .collect()
+fn parse_input(input: &str) -> Result<Vec<u32>> {
+    input.split(',').map(|s| Ok(s.trim().parse()?)).collect()
 }
 
-fn part1(input: &str) -> Result<usize> {
+fn part1(input: &str) -> Result<u32> {
     solve(&parse_input(input)?, 2020)
 }
 
-fn part2(input: &str) -> Result<usize> {
+fn part2(input: &str) -> Result<u32> {
     solve(&parse_input(input)?, 30_000_000)
 }
 
